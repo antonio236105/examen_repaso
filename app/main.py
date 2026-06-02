1d4c44b1918755e771d8f892f229eaa9be0f1736
Antonio Palacios Hernández <palacioshernandezantonio0489@gmail.com>
2026-06-02 17:30:24 +0200

    prueba del action v5

--- app/main.py
+++ app/main.py
@@ -11,4 +11,4 @@ app.include_router(items.router)
 # Código que se añade
 @app.get("/status")
 def version():
-    return {"status": "Palacios_Hernandez_Antonio - v.1.1.1.2"}
+    return {"status": "Palacios_Hernandez_Antonio - v.1.1.1.3"}
